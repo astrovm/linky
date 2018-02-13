24180c474d50d330346fe784bcb29b08579fd951
astro <astro@astrolince.com>
2018-02-13 20:23:36 -0300

    wip

--- api/controllers/create-url.js
+++ api/controllers/create-url.js
@@ -1,14 +1,59 @@
-createUrl: async function (req, res) {
-  const urlParams = {
-    id: req.param('id'),
-    target: req.param('target'),
-    emailAlerts: req.param('emailAlerts'),
-    teleAlerts: req.param('teleAlerts'),
-    captcha: req.param('captcha'),
-    password: req.param('password'),
-    email: req.param('email')
+module.exports = {
+
+  friendlyName: 'Create shorted URL',
+
+  description: 'Send the data to create-url helper.',
+
+  inputs: {
+    id: {
+      required: true,
+      type: 'string'
+    },
+    target: {
+      required: true,
+      type: 'string'
+    },
+    emailAlerts: {
+      required: true,
+      type: 'boolean'
+    },
+    teleAlerts: {
+      required: true,
+      type: 'boolean'
+    },
+    captcha: {
+      required: true,
+      type: 'string'
+    },
+    password: {
+      required: true,
+      type: 'string'
+    },
+    email: {
+      required: true,
+      type: 'string'
+    }
+  },
+
+  exits: {
+    success: {
+      description: 'URL was shorted successfully.',
+    }
+  },
+
+  fn: async function (inputs, exits) {
+    const urlParams = {
+      id: this.req.param('id'),
+      target: this.req.param('target'),
+      emailAlerts: this.req.param('emailAlerts'),
+      teleAlerts: this.req.param('teleAlerts'),
+      captcha: this.req.param('captcha'),
+      password: this.req.param('password'),
+      email: this.req.param('email')
+    }
+
+    const url = await sails.helpers.createUrl(urlParams)
+    return exits.success(url)
   }
 
-  const url = await sails.helpers.createUrl(urlParams)
-  return res.json(url)
-},
+}
