f1bfbab1e0a80b34ea3f14433950ba5193c3a5ed
astro <astro@astrolince.com>
2018-02-13 20:26:08 -0300

    wip

--- api/controllers/send-pass.js
+++ api/controllers/send-pass.js
@@ -1,9 +1,34 @@
-sendPass: function (req, res) {
-  const urlParams = {
-    id: req.param('id'),
-    passwordAttempt: req.param('password')
+module.exports = {
+
+  friendlyName: 'Send pass to helper',
+
+  description: 'Send the password to check pass helper.',
+
+  inputs: {
+    id: {
+      required: true,
+      type: 'string'
+    },
+    passwordAttempt: {
+      required: true,
+      type: 'string'
+    }
+  },
+
+  exits: {
+    success: {
+      description: 'Password is correct.',
+    }
+  },
+
+  fn: async function (inputs, exits) {
+    const urlParams = {
+      id: this.req.param('id'),
+      passwordAttempt: this.req.param('password')
+    }
+
+    const url = await sails.helpers.checkUrlPass(urlParams)
+    return exits.success(url)
   }
 
-  const url = await sails.helpers.checkUrlPass(urlParams)
-  return res.json(url)
 }
